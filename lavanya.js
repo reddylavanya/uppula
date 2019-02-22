53dd79434d23ebe56439b0bd11fff17b2301e4ce
reddylavanya <lavanya27111996@gmail.com>
2019-02-22 16:35:28 -0600

    last commit

--- lavanya.js
+++ lavanya.js
@@ -1,10 +1,32 @@
-function myFunction(input){
-side  = input;
-side = document.getElementById("num1").value;
-var area = parseInt(side) *parseInt(side);
-       
-alert("area is "+area);
-    
+function myFunction(input) {
+    side = input;
+    side = document.getElementById("num1").value;
+    var area = parseInt(side) * parseInt(side);
+
+    alert("area is " + area);
     return area;
-   
-    }
+
+}
+
+
+$(document).ready(function(){
+    $("#hide").click(function(){
+      $("p").hide();
+    });
+    $("#show").click(function(){
+      $("p").show();
+    });
+  });
+  
+            function loadDoc() {
+              var xhttp = new XMLHttpRequest();
+              xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                  document.getElementById("hello").innerHTML =
+                  this.responseText;
+                }
+              };
+              xhttp.open("GET", "webapps_ajax.txt", true);
+              xhttp.send();
+            }
+            
