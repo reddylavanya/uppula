db6bf1ca876c9ef5a27c904d7157a9f5a6188159
reddylavanya <46686805+reddylavanya@users.noreply.github.com>
2019-02-22 22:56:37 -0600

    Update lavanya.js

--- lavanya.js
+++ lavanya.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
               var xhttp = new XMLHttpRequest();
               xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-                  document.getElementById("hello").innerHTML =
+                  document.getElementById("demo").innerHTML =
                   this.responseText;
                 }
               };
